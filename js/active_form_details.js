771214c5d12201f579167b518a1e41057fc322e3
aarthipackiyaraj <aarthi@hope3.org>
2023-09-30 10:59:23 +0530

    code copied from dev

---
+++ js/active_form_details.js
@@ -0,0 +1,14 @@
+import { signOut, isAuthenticated} from "./authenticationVerify.js"
+
+// Signout button event listener
+document.getElementById('btn-signout').addEventListener('click', signOut);
+
+
+$(document).ready(function () {
+    if (!isAuthenticated()) {
+        window.location.href = 'login.html';
+    } else { 
+        document.body.style.visibility = 'visible';
+        
+    }
+});
