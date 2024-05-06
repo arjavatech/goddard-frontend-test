28ecbbb344d442a9a3a7b822c2f4e563ca5d9199
aarthi2910 <aarthi.arjava@gmail.com>
2024-05-06 21:45:50 +0530

    changed path in reset password page

--- js/password_show_hide.js
+++ js/password_show_hide.js
@@ -13,9 +13,11 @@ function togglePasswordVisibility(iconElement, passwordFieldSelector) {
     var type = passwordField.attr("type");
 
     if (type === "password") {
+        console.log('checking eye');
         iconElement.find("i").removeClass("fa-eye").addClass("fa-eye-slash");
         passwordField.attr("type", "text");
     } else if (type === "text") {
+        console.log('checking slash');
         iconElement.find("i").removeClass("fa-eye-slash").addClass("fa-eye");
         passwordField.attr("type", "password");
     }
